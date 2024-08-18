c52f895632b5c7abc36a22d915afa0847b935559
Christophe de Dinechin <christophe@dinechin.org>
2024-08-18 18:03:21 +0200

    ios: Release 0.7.14
    - New icon sets for DB40X, DB48X and DB50X
    - Built-in demo
    - Put the information button at the top of screen where no one taps
    
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- src/dm32/qspi_crc.h
+++ src/dm32/qspi_crc.h
@@ -1,3 +1,4 @@
 
-#define QSPI_DATA_SIZE   260084
+#define QSPI_DATA_SIZE   260092
 #define QSPI_DATA_CRC  0x000cfed6
+
--- src/dm42/qspi_crc.h
+++ src/dm42/qspi_crc.h
@@ -1,3 +1,4 @@
 
-#define QSPI_DATA_SIZE   259644
+#define QSPI_DATA_SIZE   259652
 #define QSPI_DATA_CRC  0x000cfed6
+
