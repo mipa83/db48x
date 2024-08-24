6ab8b377de950e6ff4c6a5a18a376f7436309a99
Christophe de Dinechin <christophe@dinechin.org>
2024-08-24 02:38:24 +0200

    ios: Force a screen refresh if a redraw is in progress
    When a refresh is already in progress, dispatch a refresh after a
    short delay to ensure that we get a timely screen update.
    
    Fixes: #1120, #1126
    
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- ios/DB48X/DB48X/ViewController.mm
+++ ios/DB48X/DB48X/ViewController.mm
@@ -206,6 +206,15 @@ enum FileSelectorState
             }
         }
     }
+    else
+    {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                                     (int64_t)(0.05 * NSEC_PER_SEC)),
+                       dispatch_get_main_queue(),
+                       ^{
+                           [self refresh];
+                       });
+    }
 }
 
 
